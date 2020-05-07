a28bd101708973a2694a6b9c99905e5cf493ac37
Gaston Funes <gtfunes@gmail.com>
2020-05-06 21:31:06 -0300

    Started project

---
+++ RedditClient/Views/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RedditClient
+//
+//  Created by Gastón Funes on 06/05/2020.
+//  Copyright © 2020 Gaston Funes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
