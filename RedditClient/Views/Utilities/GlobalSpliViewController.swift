9a948c3ebc4f175e3d2bd7413175b6b6667dd617
Gaston Funes <gtfunes@gmail.com>
2020-05-07 23:16:47 -0300

    Added SplitViewController subclass to avoid issues in compact mode

---
+++ RedditClient/Views/Utilities/GlobalSpliViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GlobalSpliViewController.swift
+//  RedditClient
+//
+//  Created by Gastón Funes on 07/05/2020.
+//  Copyright © 2020 Gaston Funes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GlobalSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+        
+        // We set the preferredDisplayMode
+        // property here to avoid showing the detail
+        // view first when in collapsed mode (portrait)
+        self.preferredDisplayMode = .allVisible
+    }
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        // Return true to prevent UIKit from applying its default behavior
+        return true
+    }
+}
